a25422822ecfcf14072ad2602c4f0db7de599de0
ajay <ajay786singh@gmail.com>
2014-10-30 23:16:17 +0530

    slider js file add

---
+++ wordpress/wp-content/themes/cafe/js/build/owl.carouse-function-call.js
@@ -0,0 +1,47 @@
+	jQuery(document).ready(function(){
+  		jQuery('#slider1').owlCarousel({
+    		loop:true,
+    		merge:true,
+    		margin:0,
+    		nav:true,
+    		dots: false,
+    		lazyLoad : true,
+    		video: true,
+    		center:true,
+    		responsive:{
+        		0:{
+            		items:1
+        		},
+        		600:{
+            		items:1
+        		},
+        		1000:{
+            		items:1
+        		}
+    		}
+		});
+
+    jQuery('#slider2').owlCarousel({
+            loop:true,
+            merge:true,
+            margin:0,
+            nav:true,
+            dots: false,
+            lazyLoad : true,
+            video: true,
+            center:true,
+            responsive:{
+                0:{
+                    items:1
+                },
+                600:{
+                    items:3
+                },
+                1000:{
+                    items:5
+                }
+            }
+        });
+
+
+	});	
