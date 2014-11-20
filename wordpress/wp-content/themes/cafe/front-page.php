ef7e8aafa66e1283efda87c36e127a7df1fb1468
ajay <ajay786singh@gmail.com>
2014-11-20 21:15:33 +0530

    add author name to feed

--- wordpress/wp-content/themes/cafe/front-page.php
+++ wordpress/wp-content/themes/cafe/front-page.php
@@ -1,9 +1,11 @@
 <?php get_header(); ?>
 
 <div id="slider2" class="owl-carousel" style="background-color: #f7f6ed">
-
-
-
+	<div class="first-element element-item item" data-category="transition">
+		<div style="overflow: hidden;width: 100%;height: 402px; background-image:url(<?php bloginfo('template_url');?>/images/feed-dummy.png); background-repeat:no-repeat; background-size: 100%; background-position: center center;">
+			<img class="element-img" src="<?php bloginfo('template_url');?>/images/feed-dummy.png" style="display:none;">
+		</div>
+	</div>
 <?php           
 	$feeds = array(
 		array('label'=>'facebook','link'=>'http://www.facebook.com/feeds/page.php?id=145253518873861&format=rss20','filter'=>'social'),
