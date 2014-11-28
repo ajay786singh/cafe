33b46fa6e8b277024c742023478591afbd09faf5
ajay <ajay786singh@gmail.com>
2014-11-28 22:55:03 +0530

    remove space

--- wordpress/wp-content/themes/cafe/header.php
+++ wordpress/wp-content/themes/cafe/header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
-    <title><?php wp_title( '|', true, 'right' );?><?php bloginfo('name');?></title>
+    <title><?php wp_title('|', true, 'right');?><?php bloginfo('name');?></title>
     <?php
         $args   =array('post_type' => 'post','posts_per_page' => 1);query_posts($args);
         if (have_posts()) : while(have_posts()) : the_post();
